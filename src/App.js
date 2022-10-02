a591cced3ed47674a80d97ffca5f3c055a9074a5
MatiasOjala <matias982@gmail.com>
2022-10-02 20:01:47 +0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Todotable from './components/Todolist';
 
 function App() {
+  const [todo, setTodo] = useState({desc: '', date: ''});
+  const [todos, setTodos] = useState([]);
+
+  const inputChanged = (event) => {
+    setTodo({...todo, [event.target.name]: event.target.value});
+  }
+
+  const addTodo = (event) => {
+    event.preventDefault();
+    setTodos([...todos, todo]);
+
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={addTodo}>
+        <h1>Todolist</h1>
+        <label htmlForm="date">Date: </label>
+          <input type="date" name="date" value={todo.date} onChange={inputChanged} />
+
+          <label htmlForm="desc">Description: </label>
+          <input type="text" name="desc" value={todo.desc} onChange={inputChanged} />
+          
+          <input type="submit" value="Add" />
+      </form>
+      <Todotable todos={todos}/>
     </div>
   );
 }
---
+++ src/components/Todolist.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default function Todolist(props) {
+    return(
+        <div>
+      <table>
+        <tbody>
+            <tr><td>Date</td><td>Description</td></tr>
+          {
+            props.todos.map((todo, index) => 
+              <tr key={index}>
+                <td>{todo.date}</td>
+                <td>{todo.desc}</td>
+              </tr>
+            )
+          }
+        </tbody>
+      </table>
+        </div>
+    );
+}
